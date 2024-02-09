559e05906ddf0f62e3ff790ed71364e43d56c084
pfcv99 <pedrofcvenancio@ua.pt>
2024-02-09 14:03:40 +0000

    new run

--- app/components/plots.py
+++ app/components/plots.py
@@ -6,12 +6,17 @@ import pandas as pd
 from pathlib import Path
 
 def plot_depth_pos(output_folder):
-    tabs = st.tabs([file.stem for file in output_folder])
-    
-    for i, tab in enumerate(tabs):
-        data = pd.read_csv(output_folder[i])
+    for file_path in Path(output_folder).glob("output_1110366_PKD1.depth"):
+        data = pd.read_csv(file_path, sep="\t")
         df = pd.DataFrame(data)
-        fig = px.scatter(df, x=df.columns[0], y=df.columns[1], marginal_x="histogram", marginal_y="rug")
+        
+        # Create a scatter plot using Plotly Express
+        fig = px.scatter(df, x=df.columns[1], y=df.columns[2], marginal_x="histogram", marginal_y="rug")
+        
+        # Add a new x-axis
+        fig.update_layout(xaxis2=dict(title=df.columns[0]))
+        
+        # Display the plot using Streamlit
         with st.container():
-            st.write(f"### {tab}")  # Display the file name as a header
-            st.plotly_chart(fig, use_container_width=True, theme="plotly")
+            st.plotly_chart(fig, use_container_width=True, theme="streamlit")
+
