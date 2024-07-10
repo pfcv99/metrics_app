e328bc6469567ef675b704e07a29b014af008247
pfcv99 <pedrofcvenancio@ua.pt>
2024-07-10 22:44:13 +0100

    minor updates on login

--- app/Home.py
+++ app/Home.py
@@ -1,15 +1,18 @@
 import streamlit as st
 from components import logo
 
+# Display logos
 sidebar_logo = "data/img/unilabs_logo.png"
 main_body_logo = "data/img/thumbnail_image001.png"
 st.logo(sidebar_logo, icon_image=main_body_logo)
 
+# Initialize session state
 if "user" not in st.session_state:
     st.session_state.user = None
     
 if "password" not in st.session_state:
     st.session_state.password = None
+
 
 @st.experimental_dialog("Log in")
 def login():
@@ -28,10 +31,12 @@ def logout():
     st.session_state.password = None
     st.rerun()
 
+
+# Retrieve user credentials
 user = st.session_state.user
 password = st.session_state.password
 
-
+# Define pages
 logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 settings = st.Page("app_pages/settings.py", title="Settings", icon=":material/settings:")
 form = st.Page("app_pages/form.py", title="Query", icon=":material/analytics:",default=((user == "userA" and password == "userA") or (user == "admin" and password == "admin")))
@@ -39,21 +44,32 @@ results = st.Page("app_pages/results.py", title="Results", icon=":material/table
 gene_panel_creator = st.Page("app_pages/gene_panel_creator.py", title="Gene panel creator", icon=":material/edit_note:",default=(user == "userB" and password == "userB"))
 about = st.Page("app_pages/3_About.py", title="About", icon=":material/info:")
 
+# Group pages
 account_pages = [logout_page, settings]
 metrics_pages = [form, results]
 panel_builder_pages = [gene_panel_creator]
 about_pages = [about]
 
+# Set up credentials
+credentials = (user, password)
+
+admin_credentials = (st.secrets.admin.user, st.secrets.admin.password)
+userA_credentials = (st.secrets.userA.user, st.secrets.userA.password)
+userB_credentials = (st.secrets.userB.user, st.secrets.userB.password)
+
+# Build page dictionary based on user credentials
 page_dict = {}
-if (st.session_state.user in [st.secrets.admin.user] and st.session_state.password in [st.secrets.admin.password]) or (st.session_state.user in [st.secrets.userA.user] and st.session_state.password in [st.secrets.userA.password]):
+if credentials in [admin_credentials, userA_credentials]:
     page_dict["Metrics calculator"] = metrics_pages
-if (st.session_state.user in [st.secrets.admin.user] and st.session_state.password in [st.secrets.admin.password]) or (st.session_state.user in [st.secrets.userB.user] and st.session_state.password in [st.secrets.userB.password]):
+if credentials in [admin_credentials, userB_credentials]:
     page_dict["Gene panels"] = panel_builder_pages
-if (st.session_state.user in [st.secrets.admin.user] and st.session_state.password in [st.secrets.admin.password]) or (st.session_state.user in [st.secrets.userA.user] and st.session_state.password in [st.secrets.userA.password]) or (st.session_state.user in [st.secrets.userB.user] and st.session_state.password in [st.secrets.userB.password]):
+if credentials in [admin_credentials, userA_credentials, userB_credentials]:
     page_dict["About"] = about_pages
-    
-if len(page_dict) > 0:
+
+
+# Display navigation
+if page_dict:
     pg = st.navigation({"Account": account_pages} | page_dict)
 else:
-    pg = st.navigation([st.Page(login)])
+    pg = st.navigation([st.Page(login, title="Unilabs app | Log in", icon=":material/login:")])
 pg.run()
