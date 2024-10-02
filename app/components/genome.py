22e11fd975b753c1e33e8fe367da6024211e659a
pfcv99 <pedrofcvenancio@ua.pt>
2024-10-02 14:05:47 +0100

    README updated

--- app/components/genome.py
+++ app/components/genome.py
@@ -1,32 +1,51 @@
+import os
+import glob
 import pandas as pd
 import streamlit as st
 
 @st.cache_data
+def find_equivalent_file(directory, pattern):
+    """
+    This function searches for files in a directory that match a specific pattern.
+    For example, '*.BED' will find any BED file in the directory.
+    """
+    files = glob.glob(os.path.join(directory, pattern))
+    if files:
+        return files[0]  # Return the first matching file
+    else:
+        raise FileNotFoundError(f"No files matching pattern {pattern} found in {directory}")
+
+@st.cache_data
 def assembly(assembly, analysis):
-    paths = {
-        "GRCh38/hg38": {
-            "Single Gene": 'data/regions/genome_exons/hg38_Twist_ILMN_Exome_2.0_Plus_Panel_annotated_modif_nochr.BED',
-            "Gene Panel": 'data/regions/genome_exons/hg38_Twist_ILMN_Exome_2.0_Plus_Panel_annotated_modif.BED',
-            "Exome": 'data/regions/genome_exons/hg38_Twist_ILMN_Exome_2.0_Plus_Panel_annotated_modif_nochr.BED'
-        },
-        "GRCh37/hg19": {
-            "Single Gene": 'data/regions/genome_exons/hg19_Twist_ILMN_Exome_2.0_Plus_Panel_annotated_modif_nochr.BED',
-            "Gene Panel": 'data/regions/genome_exons/hg19_Twist_ILMN_Exome_2.0_Plus_Panel_annotated_modif.BED',
-            "Exome": 'data/regions/genome_exons/hg19_Twist_ILMN_Exome_2.0_Plus_Panel_annotated_modif_nochr.BED'
-        }
+    directories = {
+        "GRCh38/hg38": 'data/regions/universal_bed/GRCh38_hg38/',
+        "GRCh37/hg19": 'data/regions/universal_bed/GRCh37_hg19/'
     }
 
-    path = paths.get(assembly, {}).get(analysis)
-    if path:
+    # Define patterns for searching files
+    patterns = {
+        "Single Gene": "*.BED",
+        "Gene Panel": "*.BED",
+        "Exome": "*.BED"
+    }
+
+    # Get the appropriate directory and pattern
+    directory = directories.get(assembly)
+    pattern = patterns.get(analysis)
+
+    if directory and pattern:
+        # Find the first matching file in the directory
+        path = find_equivalent_file(directory, pattern)
         return path, pd.read_csv(path, sep='\t', header=None)
     else:
         raise ValueError("Invalid assembly or analysis type")
-    
+
+
 @st.cache_data
 def panel():
-    paths = {
-        "Gene Panel" : 'data/regions/gene_panels/BED_Files_Emedgene_2.xlsx'
-        }
-    path = paths.get("Gene Panel")
-    if path:
-        return pd.read_excel(path, header=0)
+    directory = 'data/regions/gene_panels/'
+    pattern = "*.xlsx"
+
+    # Find the first matching Excel file in the directory
+    path = find_equivalent_file(directory, pattern)
+    return pd.read_excel(path, header=0)
